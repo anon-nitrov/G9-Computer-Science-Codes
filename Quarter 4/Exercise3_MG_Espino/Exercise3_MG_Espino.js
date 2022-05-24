900a0c5a38e495e3f7b0067d7941ef30dd102fe2
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-05-24 17:50:30 +0800

    Added <table> border styles \\ Added limits to given values

--- Quarter 4/Exercise3_MG_Espino/Exercise3_MG_Espino.js
+++ Quarter 4/Exercise3_MG_Espino/Exercise3_MG_Espino.js
@@ -1,6 +1,21 @@
 let countRows       =   document.getElementById("countRows").value;
 let countColumns    =   document.getElementById("countColumns").value;
+let count           =   [countRows, countColumns];
+let table   =   "<table>";
 
 function multiplicationTable(countRows, countColumns) {
+    if (count <= 2 || count >= 10) {
+        window.alert("Please enter values between 2 and 10 ONLY.");
+        return;
+    }
 
+    if (count === "") {
+        window.alert("One or Two of the cells are empty!");
+        return;
+    }
+
+    if (Number.isNaN(count) == false) {
+        window.alert("Input must only be in numbers!")
+        return;
+    }
 }
