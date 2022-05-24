348e9c3c3f15e489339bb84b352d7261709fc50d
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-05-24 17:24:46 +0800

    Added input attributes

---
+++ Quarter 4/Exercise3_MG_Espino/Exercise3_MG_Espino.js
@@ -0,0 +1,6 @@
+let countRows       =   document.getElementById("countRows").value;
+let countColumns    =   document.getElementById("countColumns").value;
+
+function multiplicationTable(countRows, countColumns) {
+
+}
