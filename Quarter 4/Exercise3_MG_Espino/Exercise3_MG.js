187b09d11f7e1064870d73f7a2e18a125e60d93d
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-05-25 12:19:30 +0800

    Added comments for better readability

--- Quarter 4/Exercise3_MG_Espino/Exercise3_MG.js
+++ Quarter 4/Exercise3_MG_Espino/Exercise3_MG.js
@@ -34,35 +34,14 @@ function multiplicationTable() {
         }
         // If any of the given input is not a number, throw an error
 
-        for (len; len   <   num; len++) {
+        for (len = 1; len   <=   count.Rows; len++) {
             table += "<tr>";
-            for (count.Rows  =   1, count.Rows    <   count.Columns; count.Rows++;) {
+            // Makes "count.Rows" number of rows for the table
+            for (count.Rows  =   1, count.Rows    <=   count.Columns; count.Rows++;) {
                 table   +=  "<td>" + count.Rows * len + "</td>";
             } table += "</tr>";
         }
     }
-
-    // if (count <= 2 || count >= 10) {
-    //     window.alert("Please enter values between 2 and 10 ONLY.");
-    //     return;
-    // }
-
-    // if (count == "" || count == 0) {
-    //     window.alert("One or Two of the cells are empty!");
-    //     return;
-    // }
-
-    // // if (isNaN(count) == true) {
-    // //     window.alert("Input must only be in numbers!")
-    // //     return;
-    // // }
-
-    // for (len; len   <   count; len++) {
-    //     table += "<tr>";
-    //     for (count.Rows  =   1, count.Rows    <   count.Columns; count.Rows++;) {
-    //         table   +=  "<td>" + count.Rows * len + "</td>";
-    //     } table += "</tr>";
-    // }
 
     table   +=  "</table>";
     document.getElementById("multiplicationTable").innerHTML = table;
